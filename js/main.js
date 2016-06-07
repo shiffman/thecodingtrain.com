7e0b4219c1508cb7b91c2f67982b2ae5d4df62c7
Scott Wooden <scott@floxx.com>
2016-06-07 18:23:12 +0100

    Various polishes and tweaks

--- js/main.js
+++ js/main.js
@@ -31,42 +31,30 @@ function getLatest() {
 }
 
 function buildPlayer(id, el) {
-
+	   
 	   player = new YT.Player(el, {
 	      height: '390',
 	      width: '640',
 	      videoId: id,
 	      events: {
 	        'onReady': onPlayerReady,
-	        'onStateChange': onPlayerStateChange
 	      }
 	    });
 	  }
 
 	  function onPlayerReady(event) {
-	    // event.target.playVideo();
 	    $(".latest-container").fitVids();
 	    $(".latest-container").addClass('in');
 	  }
 
-	  // 5. The API calls this function when the player's state changes.
-	  //    The function indicates that when playing a video (state=1),
-	  //    the player should play for six seconds and then stop.
-	  var done = false;
-	  function onPlayerStateChange(event) {
-	   if (event.data == YT.PlayerState.PLAYING && !done) {
-	     setTimeout(stopVideo, 6000);
-	     done = true;
-	   }
-	  }
 	  function stopVideo() {
-	   player.stopVideo();
+	    player.stopVideo();
 	  }
 
 function getPlaylists() {
 
 	for (var i = 0; i < shiffmanPlaylists.length; i++) {
-
+		
 		$.get('https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyADOKEHZag2UMG52bd7ApxDOssdzVo0j8I', { part: 'snippet' , id: shiffmanPlaylists[i]})
 		.done(function(data){
 
@@ -100,7 +88,7 @@ function populateData(data) {
 
 function videoList(target, playlist) {
 
-	var limit = 2;
+	var limit = 3;
 
 	$.get('https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyADOKEHZag2UMG52bd7ApxDOssdzVo0j8I', {part: 'snippet', playlistId: playlist.id} )
 	.done(function(data){
@@ -109,13 +97,12 @@ function videoList(target, playlist) {
 
 				console.log(item)
 
-				if (index != limit) {
+				if (index < limit) {
 
 					var vidSnip = item.snippet;
 
 					var html = '<a href="http://youtube.com/video/'+vidSnip.resourceId.videoId+'" target="_blank" class="feature-video">'+
-					        			'<div class="thumb">'+
-					        				'<img src="'+vidSnip.thumbnails.medium.url+'" width="100%"/>'+
+					        			'<div class="thumb" style="background-image: url('+vidSnip.thumbnails.high.url+');">'+
 					        			'</div>'+
 					        			'<div class="feature-description">'+
 					        				'<h3>'+vidSnip.title+'</h3>'+
@@ -123,9 +110,9 @@ function videoList(target, playlist) {
 					        	'</a>';
 
 					$(target).append(html);
-
+					
 				}
-
+			
 			});
 
 	}, 'JSON');
